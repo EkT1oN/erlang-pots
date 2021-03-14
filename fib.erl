d54f538bbbf5c528d99530034530c83f0e868b82
Ilgiz <38355923+EkT1oN@users.noreply.github.com>
2021-03-14 14:54:20 +0300

    Add timer exec

--- fib.erl
+++ fib.erl
@@ -5,7 +5,8 @@
     fib/1, 
     fib_p/1, 
     fib_g/1,
-    fib_tail/1
+    fib_tail/1,
+    timer_exec/2
 ]).
 
 fib(0) ->
@@ -42,3 +43,21 @@ fib_help(0, N1, _) ->
     N1;
 fib_help(C, N1, N2) ->
     fib_help(C-1, N1+N2, N1).
+
+timer_exec(Function, Argument) ->
+
+    Start = os:timestamp(),
+
+    io:format("Function: ~p, Argument:~p~n", [Function, Argument]),
+    if Function == 'fib' ->
+        Result = fib(Argument);
+    Function == 'fib_p' ->
+        Result = fib_p(Argument);
+    Function == 'fib_g' ->
+        Result = fib_g(Argument);
+    true ->
+        Result = fib_tail(Argument)
+    end,
+    
+    io:format("Exec time: ~p s~n", [timer:now_diff(os:timestamp(), Start) / 1000000]),
+    io:format("Result: ~p~n", [Result]).
